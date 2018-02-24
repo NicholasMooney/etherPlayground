9223f6765bfeaa162fd14dcdd207615855023b6f
Nicholas Mooney <nicholas.a.mooney@gmail.com>
2018-02-24 15:26:26 -0700

    updated test.js per step 47

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,7 +1,10 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
 const { interface, bytecode } = require('../compile');
 
 let accounts;
@@ -17,6 +20,7 @@ beforeEach(async () => {
     .deploy({data:bytecode, arguments:['Hi there!'] })
     .send({from: accounts[0], gas:'1000000'});
 
+    inbox.setProvider(provider);
 });
 
 describe('Inbox', () =>{
