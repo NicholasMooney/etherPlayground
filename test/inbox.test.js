dc9b178edb74e005da8728136322888061df72f0
Nicholas Mooney <nicholas.a.mooney@gmail.com>
2018-02-24 11:27:53 -0700

    adding first tests for actual project

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -3,8 +3,24 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
+beforeEach(() =>{
+    //get a list of all accounts
+    web3.eth.getAccounts()
+        .then(fetchedAccounts => {
+            console.log(fetchedAccounts);
+        });
+
+    //use one of those accounts to deploy the contract
 
 
+});
+
+describe('Inbox', () =>{
+
+    it('deploys a contract', () =>{
+        
+    });
+});
 
 
 /* commenting to save off mocha tests for future example cases. Not necessary for project
